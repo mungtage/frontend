5621b1f6d1d8dd8f7a7dbb133869eded694c90a4
cyberjam <jamsince1994@naver.com>
2022-10-10 22:20:28 +0900

    add header skeleton

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { React } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
+import Header from './components/header/Header';
 import Main from './components/Main';
 
 function App() {
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <>
+      이것은 Main :)
+      <br />
+    </>
+  );
+}
+
+export default Main;
