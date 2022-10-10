1691fd130e80d83a359082d8c39a6d441c46fe19
cyberjam <jamsince1994@naver.com>
2022-10-10 14:35:58 +0900

    🤡 기본 router 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { React } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
