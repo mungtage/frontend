4545b2d5bcaba9167638bf515848057b3fbd899a
cyberjam <jamsince1994@naver.com>
2022-10-13 21:00:17 +0900

    LostPost에서 사용자가 작성하는 부분인 LostForm.js 생성

--- src/components/lostPostTab/LostPost.js
+++ src/components/lostPostTab/LostPost.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import ImageUpload from './ImageUpload';
+import LostForm from './LostForm';
 
 function LostPost() {
   return (
-    <div className="flex p-10 h-[85vh]">
-      <div className="flex w-[50%]">
+    <div className="flex pt-5 h-[60vh]">
+      <div className="flex w-[50%] px-2">
         <ImageUpload />
       </div>
       <div className="w-[50%] px-2">
-        <div className="h-[80%]">
-          <div>이름</div>
-          <div>실종 날짜</div>
-          <div>특이 사항</div>
-        </div>
-        <div className="flex justify-end">
-          <button type="submit">매칭등록</button>
-        </div>
+        <LostForm />
       </div>
     </div>
   );
