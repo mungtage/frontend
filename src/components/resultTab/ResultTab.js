f0af2bda1fe0161ef295a73e1662ee024aa0aedc
cofla <cofla159@naver.com>
2022-10-20 03:57:30 +0900

    [feat] 신고 조회 api 연동(테스트 미완료)

--- src/components/resultTab/ResultTab.js
+++ src/components/resultTab/ResultTab.js
@@ -1,16 +1,32 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import MatchResult from './MatchResult';
 import NoResult from './NoResult';
 import NoPost from './NoPost';
 
 function ResultTab() {
-  function resultContent(matchState) {
-    if (matchState === 'matchResult') {
+  const [matchState, setMatchState] = useState('noPost');
+  const getLost = useCallback(async () => {
+    const response = await axios.get(`https://mungtage.shop/api/v1/lost`, {
+      headers: {
+        Auth: 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqaGRsMDE1NzVAZ21haWwuY29tIiwicm9sZSI6IlVTRVIiLCJpYXQiOjE2NjYxNTEyNTYsImV4cCI6MTY2NjE1MTg1Nn0.F-J2wBCWxV2yF_rs0J1mx_bv5-5OjYR5WAG4Zdqe1rA',
+        'Content-Type': `application/json`,
+      },
+    });
+    console.log(response);
+    setMatchState(response.data);
+  });
+  useEffect(() => {
+    getLost();
+  }, []);
+
+  function resultContent(state) {
+    if (state === 'matchResult') {
       return <MatchResult />;
     }
-    if (matchState === 'noResult') {
+    if (state === 'noResult') {
       return <NoResult />;
     }
     return <NoPost />;
@@ -29,7 +45,7 @@ function ResultTab() {
         </button>
       </Link>
 
-      {resultContent('matchResult')}
+      {resultContent(matchState)}
     </>
   );
 }
