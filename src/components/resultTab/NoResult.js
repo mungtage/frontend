6688d95577abc93f8e6e80f1eb05df2ed8aa4a26
cofla <cofla159@naver.com>
2022-10-22 02:59:49 +0900

    [style] noResult 스티커 추가

--- src/components/resultTab/NoResult.js
+++ src/components/resultTab/NoResult.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 function NoResult() {
   return (
-    <div className="flex-col p-20">
-      <div className="text-lg">
+    <div className="flex flex-col h-[60vh] items-center justify-center">
+      <lottie-player
+        src="https://assets2.lottiefiles.com/packages/lf20_mroczavb.json"
+        background="transparent"
+        speed="2"
+        style={{ width: '70%', height: '70%' }}
+        loop
+        autoplay
+      />
+      <div className="pb-2">
         아직 매치 완료된 공고가 없습니다. 조금만 기다려주세요.
       </div>
-      <div className="text-gray-400">
+      <div className="text-gray-400 text-sm">
         매치 완료시에는 메일로 알림이 전송됩니다.
       </div>
     </div>
