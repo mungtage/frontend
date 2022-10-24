dbc3128f4b41ee8479c0454f03c33ad4f16f626e
cyberjam <jamsince1994@naver.com>
2022-10-24 15:23:24 +0900

    tailwind alias 설정 (bg image, colors)

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
         yellow: '#ffd149',
         orange: '#ffa000',
         brown: '#c67100',
+        folder: '#fcd46c',
+        'folder-disabled-tab': '#dcb48c',
+        'point-color': '#fb94ad',
+      },
+      backgroundImage: {
+        'upload-thumb-gif': "url('https://i.ibb.co/Yd2HVfX/cat-uploader.gif')",
+        'upload-thumb': "url('https://i.ibb.co/nDxW8Vh/cat-uploader-1.png')",
       },
     },
   },
