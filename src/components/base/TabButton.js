b492d4f1e1a3c2b4b62f8e397c614aada334290e
cofla <cofla159@naver.com>
2022-10-26 06:25:14 +0900

    탭 색상 및 위치 조정

--- src/components/base/TabButton.js
+++ src/components/base/TabButton.js
@@ -7,7 +7,7 @@ function TabButton() {
       <Link to="/frontend">
         <button
           type="button"
-          className="border-x-[15px] border-b-[35px] border-solid	border-x-transparent border-b-[#fcd46c] h-0 w-[120px] ml-7 leading-9"
+          className="border-x-[15px] border-b-[35px] border-solid	border-x-transparent border-b-[#fcd46c] h-0 w-[120px] ml-10 leading-9 relative before:content-none before:block before:mt-[-7px] before:h-[15px] before:w-[120px] before:rounded-3xl before:bg-[#fcd46c] z-10"
         >
           실종 등록
         </button>
@@ -15,7 +15,7 @@ function TabButton() {
       <Link to="/frontend/results">
         <button
           type="button"
-          className="border-x-[15px] border-b-[35px] border-solid	border-x-transparent border-b-[#fcd46c] h-0 w-[120px] leading-9"
+          className="border-x-[15px] border-b-[35px] border-solid	border-x-transparent border-b-[#dcb48c] h-0 w-[120px]  leading-9 relative before:content-none before:block before:mt-[-7px] before:h-[15px] before:w-[120px] before:rounded-3xl before:bg-[#dcb48c] left-[-12px]"
         >
           매칭 결과
         </button>
