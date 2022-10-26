394db13eaabc2f4dbf7498fc2135a59f19a677d7
cyberjam <jamsince1994@naver.com>
2022-10-27 00:48:10 +0900

    헤더와 홈화면 간격 조정

--- src/components/lostPostTab/LostPost.js
+++ src/components/lostPostTab/LostPost.js
@@ -10,7 +10,7 @@ function LostPost() {
   };
 
   return (
-    <>
+    <div className="mt-[50px]">
       <TabButton />
       <div className="flex h-[60vh] bg-folder p-[40px] rounded-lg shadow-lg">
         <div className="flex w-[50%] pr-[20px]">
@@ -20,7 +20,7 @@ function LostPost() {
           <LostForm imageURL={imageURL} />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
