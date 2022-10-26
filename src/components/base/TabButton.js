80c9b60cc7d478f1560bbb3fa81c74f18a91a79a
cyberjam <jamsince1994@naver.com>
2022-10-26 21:01:46 +0900

    탭 버튼 둥글게 수정

--- src/components/base/TabButton.js
+++ src/components/base/TabButton.js
@@ -8,7 +8,7 @@ function TabButton() {
       <Link to={process.env.PUBLIC_URL}>
         <button
           type="button"
-          className={`border-x-[15px] border-b-[35px] border-solid	border-x-transparent border-b-[#fcd46c] h-0 w-[120px] ml-10 leading-9 relative before:content-none before:block before:mt-[-7px] before:h-[15px] before:w-[120px] before:rounded-3xl before:bg-[#fcd46c] ${
+          className={`font-sans border-x-[15px] border-b-[35px] border-solid	border-x-transparent border-b-[#fcd46c] rounded-t-3xl h-0 w-[120px] ml-10 leading-9 relative before:content-none before:block before:mt-[-7px] before:h-[15px] before:w-[120px] before:rounded-3xl before:bg-[#fcd46c] ${
             position ? '' : 'z-10'
           }`}
         >
@@ -18,7 +18,7 @@ function TabButton() {
       <Link to={`${process.env.PUBLIC_URL}/results`}>
         <button
           type="button"
-          className={`border-x-[15px] border-b-[35px] border-solid	border-x-transparent border-b-[#dcb48c] h-0 w-[120px]  leading-9 relative before:content-none before:block before:mt-[-7px] before:h-[15px] before:w-[120px] before:rounded-3xl before:bg-[#dcb48c] left-[-12px] ${
+          className={`font-sans border-x-[15px] border-b-[35px] border-solid	border-x-transparent border-b-[#dcb48c] rounded-t-3xl h-0 w-[120px]  leading-9 relative before:content-none before:block before:mt-[-7px] before:h-[15px] before:w-[120px] before:rounded-3xl before:bg-[#dcb48c] left-[-12px] ${
             position ? 'z-10' : ''
           }`}
         >
