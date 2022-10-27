16aed3cc10e3f468f66d9b579e815f5d74e11f97
cofla <cofla159@naver.com>
2022-10-27 22:37:13 +0900

    페이지 컴포넌트 생성

---
+++ src/pages/MatchResult.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TabButton from '../components/base/TabButton';
+import ResultTab from '../components/resultTab/ResultTab';
+
+function MatchResult() {
+  return (
+    <>
+      <TabButton />
+      <ResultTab />
+    </>
+  );
+}
+
+export default MatchResult;
