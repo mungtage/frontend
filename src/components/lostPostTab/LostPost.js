70159077442486aac2793e81b8456e78080f6c79
cyberjam <jamsince1994@naver.com>
2022-10-27 20:49:00 +0900

    탭, 홈 컨테이너 아래에 임의로 100px 패딩을 넣어 답답하기 않게 수정

--- src/components/lostPostTab/LostPost.js
+++ src/components/lostPostTab/LostPost.js
@@ -10,7 +10,7 @@ function LostPost() {
   };
 
   return (
-    <div className="mt-[50px]">
+    <div className="mt-[50px] pb-[100px]">
       <TabButton />
       <div className="flex h-[100%] bg-folder p-[40px] rounded-lg shadow-lg">
         <div className="flex w-[50%] pr-[20px]">
