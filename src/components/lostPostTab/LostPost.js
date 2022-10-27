9715ee9ccd614be44a82f3d3562f4deef9c97ea8
cyberjam <jamsince1994@naver.com>
2022-10-27 20:48:02 +0900

    폴더 높이를 상대적으로 100% 변경

--- src/components/lostPostTab/LostPost.js
+++ src/components/lostPostTab/LostPost.js
@@ -12,7 +12,7 @@ function LostPost() {
   return (
     <div className="mt-[50px]">
       <TabButton />
-      <div className="flex h-[60vh] bg-folder p-[40px] rounded-lg shadow-lg">
+      <div className="flex h-[100%] bg-folder p-[40px] rounded-lg shadow-lg">
         <div className="flex w-[50%] pr-[20px]">
           <ImageUpload onImageURL={handleImageURL} />
         </div>
