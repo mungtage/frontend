ecbea615534419a82b5b4a4a93aa589b81e241ad
cyberjam <jamsince1994@naver.com>
2022-10-29 02:51:28 +0900

    분실 등록 내역 이미지 css 조정

--- src/components/lostPostTab/IsImageUpload.js
+++ src/components/lostPostTab/IsImageUpload.js
@@ -4,14 +4,14 @@ import React from 'react';
 
 function IsImageUpload({ imageURL }) {
   return (
-    <section className="dropzone flex justify-center h-full w-full">
+    <section className="flex justify-center h-full w-full">
       <div className="flex justify-center h-full w-full items-center">
-        <div>
-          <div>
+        <div className="flex justify-center h-full w-full items-center">
+          <div className="flex justify-center h-full w-[150%] items-center ">
             <img
               src={imageURL}
               alt="This is what you uploaded."
-              className="flex flex-col justify-center items-center w-full h-[50vh]"
+              className="flex flex-col justify-center items-center w-full rounded-md"
             />
           </div>
         </div>
