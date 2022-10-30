87dcf777bc98d64785095e9c276d48716f507423
cofla <cofla159@naver.com>
2022-10-30 16:17:13 +0900

    custom alert 컴포넌트 구현

---
+++ src/components/base/Alert.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+function Alert(state, message) {
+  let color;
+  let svgPath;
+  if (state === 'success') {
+    color = 'green';
+    svgPath =
+      'M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z';
+  } else if (state === 'fail') {
+    color = 'red';
+    svgPath =
+      'M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z';
+  }
+  return (
+    <div
+      id={`toast-${state}`}
+      className="flex items-center p-4 mb-4 w-full max-w-xs text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+      role="alert"
+    >
+      <div
+        className={`inline-flex flex-shrink-0 justify-center items-center w-8 h-8 text-${color}-500 bg-${color}-100 rounded-lg dark:bg-${color}-800 dark:text-${color}-200`}
+      >
+        <svg
+          aria-hidden="true"
+          className="w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path fillRule="evenodd" d={svgPath} />
+        </svg>
+        <span className="sr-only">{state} icon</span>
+      </div>
+      <div className="ml-3 text-sm font-normal">{message}</div>
+      <button
+        type="button"
+        className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+        data-dismiss-target={`#toast-${state}`}
+        aria-label="Close"
+      >
+        <span className="sr-only">Close</span>
+        <svg
+          aria-hidden="true"
+          className="w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+}
+
+export default Alert;
