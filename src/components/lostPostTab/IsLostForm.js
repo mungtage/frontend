6391ff906603fd224107a5eb612d09be03643d49
cofla <cofla159@naver.com>
2022-10-30 16:26:57 +0900

    import 경로 에러 수정

--- src/components/lostPostTab/IsLostForm.js
+++ src/components/lostPostTab/IsLostForm.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import axios from 'axios';
-import Alert from './Alert';
+import Alert from '../base/Alert';
 
 function IsLostForm({ formData = [] }) {
   const onDelete = async (event) => {
