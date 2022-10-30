1950512d75225088ddedca9723aab61b36bd2be5
cofla <cofla159@naver.com>
2022-10-30 20:25:19 +0900

    더보기 api 연결 완료

--- src/components/resultTab/MatchResult.js
+++ src/components/resultTab/MatchResult.js
@@ -8,8 +8,8 @@ import Waiting from '../base/Waiting';
 
 function MatchResult({ lost }) {
   const accessToken = window.localStorage.getItem('token');
-  const [pageNum, setPageNum] = useState(1);
-  const maxPage = 12;
+  const [pageNum, setPageNum] = useState(0);
+  const maxPage = 11;
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
@@ -24,7 +24,7 @@ function MatchResult({ lost }) {
     try {
       setIsLoading(true);
       const response = await axios.get(
-        `https://mungtage.shop/api/v1/match?lostId=${id}&page=${pageNumber}$size=${loadSize}`,
+        `https://mungtage.shop/api/v1/match?lostId=${id}&page=${pageNumber}&size=${loadSize}`,
         {
           headers: {
             Auth: accessToken,
@@ -41,23 +41,28 @@ function MatchResult({ lost }) {
   };
   useEffect(() => {
     console.log('pageNum: ', pageNum);
-    getResult(lost.id, pageNum, 9);
+    getResult(34, pageNum, 9);
+    // getResult(lost.id, pageNum, 9);
   }, [pageNum]);
 
   console.log(results);
   return (
     <div className="flex flex-col items-center">
-      <div className="text-lg">
+      <div className="text-lg mb-8">
         <span className="font-bold">{lost.animalName}</span>(이)와 가장 닮은
         아이들입니다.
       </div>
       {!isLoading ? (
         <>
-          <main className="w-screen mx-auto my-0 text-[0px] flex flex-wrap">
+          <main className="w-[inherit] mx-auto my-0 text-[0px] flex flex-wrap justify-center">
             <RenderList list={results} key={results.rank} />
           </main>
           {pageNum <= maxPage && (
-            <button type="button" onClick={loadMore}>
+            <button
+              type="button"
+              onClick={loadMore}
+              className="text-center rounded-lg flex-1 appearance-none px-4 py-2 m-4 bg-folder-disabled-tab font-bold placeholder-black shadow-sm text-lg focus:outline-none focus:ring-1 focus:ring-[#ffa000]   focus:border-transparent"
+            >
               더 보기
             </button>
           )}
