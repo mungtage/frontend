a5c0791bae45c16d174682cbaa8627e78a73b9b4
cyberjam <jamsince1994@naver.com>
2022-11-09 23:34:11 +0900

    등록 내역 유무에 따라 컴포넌트가 표시되도록 변경

--- src/components/lostPostTab/LostPost.js
+++ src/components/lostPostTab/LostPost.js
@@ -9,7 +9,6 @@ import Alert from '../base/Alert';
 function LostPost() {
   const [imageURL, setImageURL] = useState('');
   const [lostPost, setLostPost] = useState();
-  const [isPost, setIsPost] = useState();
 
   const handleImageURL = (url) => {
     setImageURL(url);
@@ -23,10 +22,7 @@ function LostPost() {
         },
       });
       if (response.data.length) {
-        setIsPost(true);
         setLostPost(response.data.pop());
-      } else {
-        setIsPost(false);
       }
     } catch (error) {
       Alert('fail', `통신 오류가 발생했습니다. 다시 시도해주세요: ${error}`);
@@ -37,21 +33,21 @@ function LostPost() {
     if (accessToken) {
       getLost();
     }
-  }, []);
+  }, [lostPost]);
 
   return (
     <div className="pb-[100px]">
       <div className="flex h-[100%] div-folder">
         <div className="flex w-[50%] pr-[20px]">
-          {isPost ? (
+          {lostPost ? (
             <IsImageUpload imageURL={lostPost.image} />
           ) : (
             <ImageUpload onImageURL={handleImageURL} />
           )}
         </div>
         <div className="flex w-[50%] pl-[20px]">
-          {isPost ? (
-            <IsLostForm formData={lostPost} />
+          {lostPost ? (
+            <IsLostForm formData={lostPost} setLostPost={setLostPost} />
           ) : (
             <LostForm imageURL={imageURL} />
           )}
